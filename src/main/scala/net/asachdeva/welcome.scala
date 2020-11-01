1f3dfc48ccf297f8f0aea74c0901cc29b3361855
Akshay Sachdeva <asachdeva@citrine.io>
2020-11-01 14:37:01 -0600

    cleanup

--- src/main/scala/net/asachdeva/welcome.scala
+++ src/main/scala/net/asachdeva/welcome.scala
@@ -2,18 +2,4 @@ package net.asachdeva
 
 object welcome {
   //
-  // Please join and say hello:
-  //
-  // CHAT ROOM: https://discord.gg/VYvFKC8
-  //
-  // Please git clone and build:
-  //
-  // REPOSITORY: https://github.com/jdegoes/functional-design
-  //
-  // Daily Schedule:
-  //
-  //    START  : 11:00 AM London Time
-  //    BREAK  :  2:00 PM London Time
-  //    RESUME :  1:00 PM London Time
-  //    END    :  7:00 PM London Time
 }
