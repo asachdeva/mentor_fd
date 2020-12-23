2122f073c1ff8709078846f74b7e34153b594833
Raghvendra Pratap Singh <rghu_93@hotmail.com>
2020-12-23 11:56:27 -0500

    halfway through ops

--- src/main/scala/net/asachdeva/01_adts.scala
+++ src/main/scala/net/asachdeva/01_adts.scala
@@ -2,6 +2,9 @@ package net.asachdeva
 
 import java.time.Instant
 import java.util.Date
+
+import net.asachdeva.bank.Customer.BankCustomer
+import net.asachdeva.portfolio.Currency
 /*
  * INTRODUCTION
  *
@@ -39,19 +42,21 @@ object credit_card {
     * as a music concert or film showing.
     */
   sealed trait Product
-  sealed trait PhysicalProduct extends Product
-  sealed trait DigitalProduct extends Product
-  sealed trait Event extends Product
+  object Product{
+    sealed trait PhysicalProduct extends Product
+    sealed trait DigitalProduct extends Product
+    sealed trait Event extends Product
+  }
 
-  case object Milk extends PhysicalProduct
-  case object Book extends DigitalProduct
-  case object Movie extends DigitalProduct
-  case object Concert extends Event
+//  case object Milk extends PhysicalProduct
+//  case object Book extends DigitalProduct
+//  case object Movie extends DigitalProduct
+//  case object Concert extends Event
 
   /// this actually looks like I'm doing straight up polymorphism
 
 
-//  type Product
+
 
   /** EXERCISE 3
     *
@@ -92,40 +97,25 @@ object events {
 
   // Events are either UserEvent (produced by a user) or DeviceEvent (produced by a device),
   // please don't extend both it will break code!!!
-  trait UserEvent extends Event {
-    def userName: String
+  object Event {
+    sealed trait UserEvent extends Event {
+      def userName: String
+    }
+    object UserEvent {
+      final case class UserPurchase(id: Int, item: String, price: Double, time: Instant, userName: String) extends UserEvent
+      final case class UserAccountCreated(id: Int, userName: String, time: Instant) extends UserEvent
+    }
+    // Events are either UserEvent (produced by a user) or DeviceEvent (produced by a device),
+    // please don't extend both it will break code!!!
+    trait DeviceEvent extends Event {
+      def deviceId: Int
+    }
+
+    object DeviceEvent {
+      final case class SensorUpdated(id: Int, deviceId: Int, time: Instant, reading: Option[Double]) extends DeviceEvent
+      final case class DeviceActivated(id: Int, deviceId: Int, time: Instant) extends DeviceEvent
+    }
   }
-
-  // Events are either UserEvent (produced by a user) or DeviceEvent (produced by a device),
-  // please don't extend both it will break code!!!
-  trait DeviceEvent extends Event {
-    def deviceId: Int
-  }
-
-  case class SensorUpdated(
-      id: Int,
-      deviceId: Int,
-      time: Instant,
-      reading: Option[Double]
-  ) extends Event
-      with DeviceEvent
-
-  case class DeviceActivated(id: Int, deviceId: Int, time: Instant)
-      extends Event
-      with DeviceEvent
-
-  case class UserPurchase(
-      id: Int,
-      item: String,
-      price: Double,
-      time: Instant,
-      userName: String
-  ) extends Event
-      with UserEvent
-
-  case class UserAccountCreated(id: Int, userName: String, time: Instant)
-      extends Event
-      with UserEvent
 }
 
 /** DOCUMENT EDITING - EXERCISE SET 4
@@ -143,9 +133,10 @@ object documents {
     * Using only sealed traits and case classes, create a simplified but somewhat
     * realistic model of a Document.
     */
-  type Document
-  final case class DocumentList(userId: UserId, docId: DocId, content: DocContent) {
-
+//  type Document
+  sealed trait Document
+  object Document{
+    final case class DocumentList(userId: UserId, docId: DocId, content: DocContent) extends Document
   }
 
   /** EXERCISE 2
@@ -154,14 +145,20 @@ object documents {
     * type that a given user might have with respect to a document. For example,
     * some users might have read-only permission on a document.
     */
-  type AccessType
-  type ReadAcess
-  sealed trait Access_Type[AccessType] {
-    def docId: DocId
-    def userId: UserId
+//  type AccessType
+//  type ReadAcess
+//  sealed trait Access_Type[AccessType] {
+//    def docId: DocId
+//    def userId: UserId
+//  }
+//  final case class ReadAccess[ReadAccess](docId: DocId, userId: UserId) extends Access_Type[AccessType]
+//  final case class WriteAccess(docId:DocId, userId: UserId, content: DocContent) extends Access_Type[AccessType]
+  sealed trait AccessType
+  object AccessType{
+    final case object NoAccess extends AccessType
+    final case object ReadAccess extends AccessType
+    final case object WriteAccess extends AccessType
   }
-  final case class ReadAccess[ReadAccess](docId: DocId, userId: UserId) extends Access_Type[AccessType]
-  final case class WriteAccess(docId:DocId, userId: UserId, content: DocContent) extends Access_Type[AccessType]
 
   /** EXERCISE 3
     *
@@ -169,10 +166,11 @@ object documents {
     * permissions that a user has on a set of documents they have access to.
     * Do not store the document contents themselves in this model.
     */
-  type DocPermissions
-  sealed trait Permissions[DocPermissions]{
-    def givePermission
-  }
+//  type DocPermissions
+//  sealed trait Permissions[DocPermissions]{
+//    def givePermission
+//  }
+  final case class Permission(userId: UserId, permissionsMap: Map[DocId, AccessType])
 }
 
 /** BANKING - EXERCISE SET 5
@@ -185,7 +183,10 @@ object bank {
     *
     * Using only sealed traits and case classes, develop a model of a customer at a bank.
     */
-  type Customer
+  sealed trait Customer
+  object Customer{
+    final case class BankCustomer(id: Long, name: String, accNo: Long, accType: String) extends Customer
+  }
 
   /** EXERCISE 2
     *
@@ -193,8 +194,15 @@ object bank {
     * type. For example, one account type allows the user to write checks
     * against a given currency. Another account type allows the user to earn
     * interest at a given rate for the holdings in a given currency.
-    */
-  type AccountType
+    * */
+//  type AccountType
+  sealed trait AccountType
+  object AccountType{
+    final case class InterestChecking(interestRate: Double) extends AccountType
+    final case class Holdings(currency: String) extends AccountType
+    final case object PersonalChecing extends AccountType
+  }
+
 
   /** EXERCISE 3
     *
@@ -202,7 +210,7 @@ object bank {
     * account, including details on the type of bank account, holdings, customer
     * who owns the bank account, and customers who have access to the bank account.
     */
-  type Account
+  final case class BankAccount(accType: AccountType, ownerCust: BankCustomer, accessCust: List[BankCustomer])
 }
 
 /** STOCK PORTFOLIO - GRADUATION PROJECT
@@ -216,44 +224,55 @@ object portfolio {
     * Using only sealed traits and case classes, develop a model of a stock
     * exchange. Ensure there exist values for NASDAQ and NYSE.
     */
-  type Exchange
-  sealed trait StockExchange[Exchange]{
-    def exchangeName(e: Exchange): String
-  }
-  final case class NYSE[Exchange](e: Exchange) extends StockExchange[Exchange]{
-    def exchangeName(e: Exchange): String = "NYSE"
+  sealed trait StockExchange
+  object StockExchange{
+    final case object NYSE extends StockExchange
+    final case object NASDAQ extends StockExchange
   }
 
-  // should NYSE be another class or a param inside the StockExchange trait
-  
   /** EXERCISE 2
     *
     * Using only sealed traits and case classes, develop a model of a currency
     * type.
     */
-  type CurrencyType
-  sealed trait Currency[CurrencyType]
+
+  sealed trait Currency
+  object Currency{
+    final case object USD extends Currency
+  }
 
   /** EXERCISE 3
     *
     * Using only sealed traits and case classes, develop a model of a stock
     * symbol. Ensure there exists a value for Apple's stock (APPL).
     */
-  type StockSymbol
+//  type StockSymbol
+  sealed trait StockSymbol
+  object StockSymbol{
+    final case object AAPL extends StockSymbol
+  }
 
   /** EXERCISE 4
     *
     * Using only sealed traits and case classes, develop a model of a portfolio
     * held by a user of the web application.
     */
-  type Portfolio
+//  type Portfolio
+  sealed trait Portfolio
+  object Portfolio{
+    final case class UserPortfolio(user: User, stocks: Seq[StockExchange]) extends Portfolio
+  }
+
 
   /** EXERCISE 5
     *
     * Using only sealed traits and case classes, develop a model of a user of
     * the web application.
     */
-  type User
+//  type User
+
+  final case class User(userId: Long, userName: String)
+
 
   /** EXERCISE 6
     *
